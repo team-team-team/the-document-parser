010d5bfe084775808b0da3945ce5ac88f310494b
kyle-pawlowski <kylepawlowski1@gmail.com>
2017-03-25 16:04:09 -0400

    Make word class def

---
+++ Hackathon/TextBlobTest.py
@@ -0,0 +1,8 @@
+from textblob import TextBlob
+
+def findAssociated():
+
+def makeSentences(words):
+  #words is a list of Word objects with the word as a string, (x,y) of middle,
+  #hight, and width of text box
+  for i in words:
---
+++ Hackathon/Word.py
@@ -0,0 +1,7 @@
+class Word():
+  def __init__(self, text, cord, height, width):
+    #text is a string, cord is a tuple for the (x,y) of center, height is an int, width is an int
+    self.text = text
+    self.cord = cord
+    self.height = height
+    self.width = width
